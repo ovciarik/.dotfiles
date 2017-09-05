a5cd715f1430077984a1ef129da085b251eeeb50
ovciarik <martin.ovciarik@gmail.com>
2017-09-05 19:01:31 +0100

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,33 @@
-apt-get install git
+# i3 deps
 apt-get install i3-wm
 apt-get install i3status
 apt-get install i3lock
 apt-get install suckless-tools
+apt-get install dunst
+apt-get install fonts-font-awesome
+pip install py3status
+
+# keyboard remap
+apt-get install xcape
+
+# programs
+apt-get install git
 apt-get install rxvt-unicode
 apt-get install zsh
 apt-get install ranger
-apt-get install xcape
 apt-get install neovim
-apt-get install fonts-font-awesome
 
+# ranger deps
+apt-get install file
+apt-get install caca-utils
+apt-get install highlight
+apt-get install atool
+apt-get install transmission-cli
+apt-get install mediainfo
+apt-get install unoconv
+
+# oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-pip install py3status
 
 # neovim plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
