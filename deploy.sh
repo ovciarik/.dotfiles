b853b387eb462eb845f5d5fd99212f4a4f685a43
Martin Ovciarik <martin.ovciarik@zeetta.com>
2017-09-05 11:10:50 +0100

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+apt-get install git
+apt-get install i3-wm
+apt-get install i3status
+apt-get install i3lock
+apt-get install suckless-tools
+apt-get install rxvt-unicode
+apt-get install zsh
+apt-get install ranger
+apt-get install xcape
+apt-get install neovim
+apt-get install fonts-font-awesome
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+pip install py3status
+
