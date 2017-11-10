1461a7e8987317cc950c77e4b34a64e819201a69
ovciarik <martin.ovciarik@gmail.com>
2017-11-10 20:53:26 +0000

    neovim python integration

--- .deploy.sh
+++ .deploy.sh
@@ -44,3 +44,7 @@ update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 update-alternatives --config vim
 update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 update-alternatives --config editor
+
+# neovim python integration
+pip install neovim
+pip3 install neovim
