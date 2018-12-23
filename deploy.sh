e89fc03ee282de51bb11da523b9c2fc336796b9e
ovciarik <martin.ovciarik@gmail.com>
2018-12-23 09:25:27 +0000

    don't show editor picker

--- deploy.sh
+++ deploy.sh
@@ -54,11 +54,11 @@ sudo apt-get install -y python3-pip
 sudo apt-get install -y neovim
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --config vi
+# sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --config vim
+# sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-sudo update-alternatives --config editor
+# sudo update-alternatives --config editor
 
 git clone --separate-git-dir="$HOME/.dotfiles" https://github.com/ovciarik/.dotfiles "/tmp/dotfiles"
 cp -r /tmp/dotfiles/. $HOME
