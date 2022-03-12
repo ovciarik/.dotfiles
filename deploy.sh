ddf389684501a96c2a6c064e0008d20920dcfbe8
ovciarik <martin.ovciarik@gmail.com>
2022-03-12 00:13:49 +0000

    ...

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,37 @@
 #!/bin/sh
 
+sudo pacman-mirrors --fasttrack
+sudo paman -Syu
 sudo pacman -S --noconfirm yay
+
+yay -S --noconfirm rofi
+yay -S --noconfirm gnome-themes-extra
+yay -S --noconfirm adwaita-qt
+yay -S --noconfirm adwaita-qt6
 yay -S --noconfirm neovim
+yay -R --noconfirm vi
+yay -R --noconfirm vim
 yay -S --noconfirm neovim-symlinks
 yay -S --noconfirm ranger
+yay -S --noconfirm w3m
 yay -S --noconfirm mpv
 yay -S --noconfirm pavucontrol
 yay -S --noconfirm pulseaudio
-
 yay -S --noconfirm google-chrome
 yay -S --noconfirm atool
+yay -S --noconfirm unzip
 yay -S --noconfirm visual-studio-code-bin
-
 yay -S --noconfirm xcape
 yay -S --noconfirm redshift
-
 yay -S --noconfirm blueman
+yay -S --noconfirm brightnessctl
+yay -S --noconfirm qbittorrent
+yay -S --noconfirm tlp
+yay -S --noconfirm tlp-rdw
+# enable service after installation
+sudo systemctl enable tlp.service
+systemctl enable NetworkManager-dispatcher.service
+
 
 git clone --separate-git-dir="$HOME/.dotfiles" https://github.com/ovciarik/.dotfiles "/tmp/dotfiles"
 cp -r /tmp/dotfiles/. $HOME
@@ -29,3 +45,19 @@ chsh -s $(which zsh)
 sudo usermod $(whoami) -a -G video
 
 echo "Reboot the PC, and log into i3"
+
+# paswordless sudo
+# sudo vi /etc/sudoers
+# martin ALL=(ALL:ALL) NOPASSWD:ALL
+
+# enable bluetooth + bluetooth turned off by default
+# vi /etc/bluetooth/main.conf
+# AutoEnable=false
+
+# systemctl enable bluetooth.service
+
+# etc hosts
+# sudo vi /etc/hosts
+
+# set DNS
+# sudo vi /etc/NetworkManager/confd/dns-servers.conf
